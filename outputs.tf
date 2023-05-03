e47b896f7cf1506a70a434c3ae7dda51d0e47627
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-03 11:16:39 +0530

    Add: cloudfront_log_bucket_name output

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "cloudfront_distribution_hosted_zone_id" {
   description = "CloudFront distribution hosted zone ID"
   value       = aws_cloudfront_distribution.default.hosted_zone_id
 }
+
+output "cloudfront_log_bucket_name" {
+  description = "CloudFront log bucket name"
+  value       = local.create_cloudfront_log_bucket ? aws_s3_bucket.cloudfront_log[0].id : null
+}
